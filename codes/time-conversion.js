873323efed22f5d7faddd5ad1407aa4b88a4ae25
paprika <paprika@spicegrid.net>
2019-10-15 15:52:54 +0900

    day12

---
+++ codes/time-conversion.js
@@ -0,0 +1,11 @@
+function timeConversion(s) {
+    let hours= s[0]+s[1];
+    if(s.includes('PM') && hours < 12) {
+        let time = Number(hours);
+        time = time += 12;
+        return s.replace(hours, time).slice(0,8);
+    } else {
+        if(s.includes('AM') && hours == 12) return s.replace(hours, '00').slice(0,8);
+        return s.slice(0,8);
+    }
+}
