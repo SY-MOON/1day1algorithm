f8c095a40cc91f4a130a8b0cfbde50b1b41c6870
paprika <paprika@spicegrid.net>
2019-10-15 15:55:05 +0900

    day14-1

---
+++ codes/programmers-skill-test-lev2-1.js
@@ -0,0 +1,19 @@
+function solution(s){   
+    const stack = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        const target = stack.pop();
+
+        if(!target) {
+            stack.push(s[i]);
+            continue;
+        }
+        
+        if(target != s[i]) {
+            stack.push(target, s[i]);
+            continue;
+        }
+    }
+    
+    return stack.length == 0 ? 1 : 0;
+}
