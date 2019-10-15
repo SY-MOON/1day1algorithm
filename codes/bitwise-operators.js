1b0af5be9b67bfbbe96397ca897e9f540a58aeb9
paprika <paprika@spicegrid.net>
2019-10-15 15:36:37 +0900

    day4

---
+++ codes/bitwise-operators.js
@@ -0,0 +1,15 @@
+function getMaxLessThanK(n, k) {
+    let max = 0;
+    for(let i = 1; i < n; i++) {
+        for(let j = 2; j <= n; j++) {
+            if(i < j && i != j) {
+                let bitAnd = parseInt(i & j);
+                let toDecimal = parseInt(bitAnd, 10)
+                if(toDecimal < k) {
+                    if(toDecimal > max) max = toDecimal;
+                }
+            }
+        }
+    }
+    return max;
+}
