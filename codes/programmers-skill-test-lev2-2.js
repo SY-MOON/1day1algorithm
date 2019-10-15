deb8af55aea898355e30a2d08e76d5431f77f756
paprika <paprika@spicegrid.net>
2019-10-15 15:55:30 +0900

    day14-2

---
+++ codes/programmers-skill-test-lev2-2.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    var answer = '';
+    let splited = s.toLowerCase().split(" ");
+    for(let i = 0; i < splited.length; i++) {
+        splited[i] = splited[i].charAt(0).toUpperCase() + splited[i].slice(1);
+    }
+    answer = splited.join(" ");
+    return answer;
+}
