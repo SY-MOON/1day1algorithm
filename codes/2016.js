e8536e8393ca3bfdd4b4225d8e0d0fa8480b20d2
paprika <paprika@spicegrid.net>
2019-10-16 16:59:14 +0900

    day16

---
+++ codes/2016.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    var answer = '';
+    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    
+    let setDate = new Date(2016, a-1, b);
+    answer = days[setDate.getDay()];
+    
+    return answer;
+}
