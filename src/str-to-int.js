b219acb68ccfb6cd1f3b031b4837654150c6cbe5
SY-MOON <seokyoung.moon90@gmail.com>
2019-10-22 15:47:17 +0900

    1일 1알고리즘 Day22

---
+++ src/str-to-int.js
@@ -0,0 +1,8 @@
+function solution(s) { 
+	var answer = 0; 
+	answer = Number.parseInt(s); 
+	return answer; 
+}
+
+
+module.exports = solution
---
+++ src/str-to-int.spec.js
@@ -0,0 +1,4 @@
+const solution = require('./str-to-int')
+test('주어진 문자열을 숫자로', () => {
+    expect(-1234).toBe(solution('-1234'));
+});
