650d7385dc2fdac7eb88683efcc11b71efd92e4a
SY-MOON <seokyoung.moon90@gmail.com>
2019-11-01 00:01:22 +0900

    테스트 케이스 11번 실패 문제 해결

--- src/string-control.js
+++ src/string-control.js
@@ -1,5 +1,6 @@
 function solution(s) {
     var answer = false;
+    if(Number(s) && s.includes('e')) return answer;
     (s.length == 4 || s.length ==6) && Number(s) ? answer = true : answer;
     return answer
 }
